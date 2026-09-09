9d7bfce057f1bb7fdba4d7a1e032391e1c0f427a
M Umar Adnan <umaradnan2710@gmail.com>
2026-09-09 17:08:34 +0500

    Demonstrates how to read first 5 and last 5 rows for preview and checking if the data loaded successfully or not.

---
+++ Reading Chunks.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv("online food delivery dataset.csv")
+print(df.head()) # Gives First 5 Rows and prints them
+print(df.tail()) # Gives last 5 Rows and prints them
