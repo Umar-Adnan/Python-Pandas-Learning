1541bbba9c18408d0c2d85b3bee48bfd63e7bec2
M Umar Adnan <umaradnan2710@gmail.com>
2026-09-09 15:53:44 +0500

    Demonstrates how to read files of different formats like CSV/Excel/JSON.

---
+++ Reading File.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# csv = pd.read_csv("online food delivery dataset.csv") #To read .csv files
+# print(csv)
+
+# excel_data = pd.read_excel("Lab work.xlsx") #To read .xlsx files
+# print(excel_data)
+
+json = pd.read_json("test.json") #To read json files
+print(json)
+
+
